dcdd3eddae6181e6058de71954d5481406df1a9a
fasih10 <fasih111@hotmail.com>
2019-07-15 14:04:07 +0500

    B

--- hello.java
+++ hello.java
@@ -1,7 +1,13 @@
 public class hello{
 public static void main()
 {
+<<<<<<< Updated upstream
 System.out.println("Hello worldB");
+=======
+<<<<<<< HEAD
+System.out.println("Hello worldA");
+System.out.println("A");
+>>>>>>> Stashed changes
 System.out.println("B");
 }
 }
